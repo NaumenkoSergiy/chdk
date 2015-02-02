490658ae2aa38b0565113efa7620f8c1a8451ef9
naumenko <mobilesoft@spaces.ru>
2015-02-02 22:07:50 +0200

    helper for sliders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
-	def test
-	end
+  def images_render
+    s = ""
+    @cms_site.files.images.for_category('sliders').each do |fl|
+      my_src = "/system/comfy/cms/files/files/000/000/%03d/original/%s" % [fl.id, fl.file_file_name]
+      s << image_tag(my_src, class: "pic").html_safe
+    end
+    s.html_safe
+  end
 end
